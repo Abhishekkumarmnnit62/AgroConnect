f3db7114531bef4de926d5c16af782212d29cf80
Abhishekkumarmnnit62 <abhishekkumarmnnit62@gmail.com>
2026-05-23 09:23:18 +0530

    Frontend setup with React and Tailwind

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+const Home = () => {
+  return (
+    <div className="h-screen flex items-center justify-center bg-green-100">
+      <h1 className="text-4xl font-bold text-green-700">
+        AgroConnect
+      </h1>
+    </div>
+  );
+};
+
+export default Home;
